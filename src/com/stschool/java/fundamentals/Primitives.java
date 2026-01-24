efced2f9295855fba6de63ee51a074e50f1e6523
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-24 23:42:08 +0530

    feat:Primitives

---
+++ src/com/stschool/java/fundamentals/Primitives.java
@@ -0,0 +1,84 @@
+package com.stschool.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        byte shirtSize, batteryPercentage, noOfTyres, replacementDays, discountPercentage;
+        shirtSize = 40;
+        batteryPercentage = 80;
+        noOfTyres = 4;
+        replacementDays = 7;
+        discountPercentage = 50;
+        System.out.println("Shirt size : " + shirtSize);
+        System.out.println("Battery percentage : " + batteryPercentage);
+        System.out.println("Number of tyres : " + noOfTyres);
+        System.out.println("Replacement days : " + replacementDays);
+        System.out.println("Discount percentage : " + discountPercentage);
+
+        short marks, bookPages, memorySize, batterySize, stockAvailability;
+        marks = 955;
+        bookPages = 320;
+        memorySize = 512;
+        batterySize = 4100;
+        stockAvailability = 3000;
+
+        System.out.println("Marks : " + marks);
+        System.out.println("Book pages : " + bookPages);
+        System.out.println("Memory size : " + memorySize);
+        System.out.println("Battery size : " + batterySize);
+        System.out.println("Stock availability : " + stockAvailability);
+
+        int reviewCount, price, exchangeValue, kilometers, rank;
+        reviewCount = 2646;
+        price = 77747;
+        exchangeValue = 7564;
+        kilometers = 5763;
+        rank = 104948;
+
+        System.out.println("Review count : " + reviewCount);
+        System.out.println("Price : " + price);
+        System.out.println("Exchange value : " + exchangeValue);
+        System.out.println("Kilometers : " + kilometers);
+        System.out.println("Rank : " + rank);
+
+        long mobileNumber, aadhaarNumber, accountNumber, population, netWorth;
+        mobileNumber = 9876543210L;
+        aadhaarNumber = 746675649854L;
+        accountNumber = 7458746563487567348L;
+        population = 85487589775L;
+        netWorth = 845846565765L;
+
+        System.out.println("Mobile number : " + mobileNumber);
+        System.out.println("Aadhaar number : " + aadhaarNumber);
+        System.out.println("Account number : " + accountNumber);
+        System.out.println("Population : " + population);
+        System.out.println("Net worth : " + netWorth);
+
+        float rating, uiVersion, displaySize, engineTemperature, bluetoothVersion;
+        rating = 4.8F;
+        uiVersion = 14.2F;
+        displaySize = 7.2F;
+        engineTemperature = 110.4F;
+        bluetoothVersion = 4.3F;
+
+        System.out.println("Rating : " + rating);
+        System.out.println("UI version : " + uiVersion);
+        System.out.println("Display size : " + displaySize);
+        System.out.println("Engine temperature : " + engineTemperature);
+        System.out.println("Bluetooth version : " + bluetoothVersion);
+
+        double coordinates, piValue, bloodSugarLevel, networkLatency, executionTime;
+        coordinates = 73568.34874;
+        piValue = 3.1415887348;
+        bloodSugarLevel = 67.867;
+        networkLatency = 0.704;
+        executionTime = 0.0045;
+
+        System.out.println("Coordinates : " + coordinates);
+        System.out.println("Pi value : " + piValue);
+        System.out.println("Blood sugar level : " + bloodSugarLevel);
+        System.out.println("Network latency : " + networkLatency);
+        System.out.println("Execution time : " + executionTime);
+
+    }
+}
