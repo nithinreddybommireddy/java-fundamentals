f4fe0b75bd6cd61aa84b1eaa223f423622b74b41
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-05 23:34:59 +0530

    feat: Fibonacci Series

--- src/com/java/fundamentals/FibonacciSeries.java
+++ src/com/java/fundamentals/FibonacciSeries.java
@@ -6,7 +6,7 @@ public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of terms: ");
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
 
         int a = 0, b = 1;
